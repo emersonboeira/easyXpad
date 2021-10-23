cf0d560d0c54d518daf9e5401e04438bb60e3657
Emerson Boeira <emerson.boeira@gmail.com>
2021-10-22 21:28:43 -0300

    Organizing files and folders - moving main.cpp to app/ folder

---
+++ app/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include "eXpad.h"
+
+int main()
+{
+    std::list<eXpad::XboxController> controller_list = eXpad::findXpads();
+
+    if (!controller_list.empty()) {
+        
+        std::list<eXpad::Button> controllerButtons;
+        std::list<eXpad::Button>::iterator buttonsIterator;
+
+        std::list<eXpad::Axis> controllerAxis;
+        std::list<eXpad::Axis>::iterator axisIterator;
+
+        while(1) {
+        
+            controller_list.begin()->readControllerEvents();
+            controllerButtons = controller_list.begin()->getButtons();
+            controllerAxis = controller_list.begin()->getAxis();
+
+            for (buttonsIterator = controllerButtons.begin(); buttonsIterator != controllerButtons.end(); buttonsIterator++)
+                std::cout << "(" << buttonsIterator->name+": " << buttonsIterator->value << ")";
+            std::cout << "  ";
+            for (axisIterator = controllerAxis.begin(); axisIterator != controllerAxis.end(); axisIterator++)
+                std::cout << "(" <<  axisIterator->name+": " << std::setw(6) << axisIterator->value << ")";
+            std::cout << std::endl;
+        }
+    }
+    return 0;
+}
